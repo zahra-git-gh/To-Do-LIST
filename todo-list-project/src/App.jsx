52d327837b5f04f95cc6141410796355debbde8e
zahra-gh <zhrww.zrww@gmail.com>
2024-07-01 17:53:02 +0330

    add darkmode button and bar progress

--- todo-list-project/src/App.jsx
+++ todo-list-project/src/App.jsx
@@ -1,33 +1,7 @@
 import "./App.css";
-import { Nav } from "./components/Nav/Nav";
-
-// import { Header } from "./components/Header/Header";
-
-// import { SortBySelect } from "./components/SortBySelect/SortBySelect";
-// import { ParentTaskCards } from "./components/ParentTaskCards/ParentTaskCards";
-// import { SearchInput } from "./components/Search-Input/SearchInput";
-// import { NewTaskBtn } from "./components/Add-new-task-button/NewTaskBtn";
 
 function App() {
-  // window.document.body.style.backgroundColor = "rgb(15, 23, 42)";
-  // window.document.body.style.backgroundColor='#'
-  //classnames for square cards parent
-
-  return (
-    <>
-    {/* <section className="px-4">
-
-      <ParentTaskCards isList={false}/>
-    </section> */}
-      {/* <SortBySelect /> */}
-      {/* <SearchInput /> */}
-      {/* <Header /> */}
-      <div className="h-screen">
-      <Nav/>
-
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ todo-list-project/src/components/Darkmode/Darkmode.jsx
@@ -0,0 +1,32 @@
+import "./Darkmode.css";
+export function Darkmode() {
+  function handleClickDarkmode() {
+    const html = document.getElementsByTagName("html")[0];
+    const darkBtn = document.querySelector(".darkBtn1");
+    if (html.classList.contains("dark")) {
+      darkBtn.classList.remove("darkBtn");
+      // html.classList.remove('dark')
+      html.removeAttribute("class");
+    } else {
+      darkBtn.classList.add("darkBtn");
+      // html.classList.add('dark')
+      html.setAttribute("class", "dark");
+    }
+    // darkBtn.classList.toggle('darkBtn')
+  }
+  return (
+    <section className="w-full">
+      <button
+        onClick={handleClickDarkmode}
+        className="w-full flex justify-between"
+      >
+        <span className="text-xs sm:text-sm xl:text-base text-slate-500 dark:text-slate-100">
+          Darkmode
+        </span>
+        <div className="w-10 h-5 bg-slate-200 rounded-full flex items-center dark:bg-slate-700/[.3] darkBtn1">
+          <div className="w-4 h-4 rounded-full bg-blue-700"></div>
+        </div>
+      </button>
+    </section>
+  );
+}
