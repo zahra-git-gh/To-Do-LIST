125e149b01ad74d1335b9e7f6c9853e370f6ee0c
zahra-gh <zhrww.zrww@gmail.com>
2024-07-02 15:07:24 +0330

    create task modal done

--- todo-list-project/src/App.jsx
+++ todo-list-project/src/App.jsx
@@ -1,14 +1,7 @@
 import "./App.css";
-import { EditTaskModal } from "./components/Modals/EditTaskModal/EditTaskModal";
-// import { UserProfile } from "./components/UserProfile/UserProfile";
-// import { Darkmode } from "./components/Darkmode/Darkmode";
 
 function App() {
-  return <>
-  {/* <UserProfile/> */}
-  <EditTaskModal/>
-  <p className="text-white">todolist</p>
-  </>;
+  return <></>;
 }
 
 export default App;
