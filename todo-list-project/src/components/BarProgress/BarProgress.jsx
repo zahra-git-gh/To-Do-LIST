21f658e1e8f769e5c9e2e5de5d10764f8010b831
zahra-gh <zhrww.zrww@gmail.com>
2024-07-02 11:13:39 +0330

    user profile done

--- todo-list-project/src/components/BarProgress/BarProgress.jsx
+++ todo-list-project/src/components/BarProgress/BarProgress.jsx
@@ -9,7 +9,7 @@ export function BarProgress({ allTasks, completedTasks, title }) {
         </p>
         <p className="text-xs sm:text-sm xl:text-base text-slate-500 dark:text-slate-400 ">{`${completedTasks}/${allTasks}`}</p>
       </div>
-      <div className="w-full h-2 rounded-full bg-slate-200 overflow-hidden">
+      <div className="w-full h-2 rounded-full bg-slate-200 dark:bg-slate-700 overflow-hidden">
         <div
           style={{ width: `${width.toFixed(3)}%` }}
           className="bg-blue-700 overflow-hidden h-2"
