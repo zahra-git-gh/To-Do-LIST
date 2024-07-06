2f215ccbfc9e9b604482038a0496c262a5fe27d9
zahra-gh <zhrww.zrww@gmail.com>
2024-07-06 17:55:26 +0330

    fix edit directory bug

--- todo-list-project/src/components/Modals/EditDirectoryModal/EditDirectoryModal.jsx
+++ todo-list-project/src/components/Modals/EditDirectoryModal/EditDirectoryModal.jsx
@@ -1,31 +1,23 @@
 import { useModal } from "../../../hooks/ModalContext";
 import "../EditTaskModal/EditTaskModal.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useForm } from "react-hook-form";
 import { updateDirectory } from "../../../redux/todos.slice";
+import { useEffect, useState } from "react";
 export function EditDirectoryModal({ modalIDD }) {
   const { isModalOpen, closeModal, modalID, id } = useModal();
   const directories = useSelector((state) => state.todo.directories);
   const directory = directories.filter((dir) => dir.id === id)[0];
+  const [data, setData] = useState();
   const dispatch = useDispatch();
+  useEffect(() => {
+    if (directory) {
+      setData(directory);
+    }
+  }, [directory]);
 
-  console.log(directory);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm({});
-  const helperText = {
-    editName: {
-      required: "Directory name is required",
-    },
-  };
-  console.log(errors);
-  function submitEdit(value) {
-    console.log(value);
-    dispatch(updateDirectory({ id, name: value.editName }));
-    reset();
+  function submitEdit() {
+    dispatch(updateDirectory(data));
+    setData(null);
     closeModal();
   }
   if (modalID !== modalIDD) {
@@ -50,7 +42,7 @@ export function EditDirectoryModal({ modalIDD }) {
               className="close w-6 h-6 bg-slate-600 dark:bg-slate-200"
             ></button>
           </div>
-          <form onSubmit={handleSubmit(submitEdit)}>
+          <form onSubmit={submitEdit}>
             <div className="flex flex-col gap-y-2 mt-5">
               <label
                 className="text-xs sm:text-sm text-slate-500 xl:text-base"
@@ -62,18 +54,18 @@ export function EditDirectoryModal({ modalIDD }) {
                 className="bg-slate-100 dark:bg-slate-800 dark:text-slate-200 h-12 rounded-lg focus:border-2 focus:border-blue-600 hover:border-2 hover:border-blue-600 focus:outline-none px-2 py-3 text-xs sm:text-sm xl:text-base"
                 type="text"
                 id="title"
-                placeholder="Enter a directory name"
-                {...register("editName", { required: true })}
-                defaultValue={directory && directory.name}
+                required
+                onChange={(e) => {
+                  setData({ ...data, name: e.target.value });
+                }}
+                value={data?.name}
               />
-              {errors.editName && (
-                <div className="text-sm text-red-500 font-thin">
-                  {helperText.editName[errors.editName.type]}
-                </div>
-              )}
             </div>
             <div className="mt-6 flex justify-start gap-x-3 items-center">
-              <button className="text-xs bg-[#333d91] hover:bg-[#5163ae] text-slate-100 rounded-lg py-3 px-6 sm:text-sm xl:text-base">
+              <button
+                type="submit"
+                className="text-xs bg-[#333d91] hover:bg-[#5163ae] text-slate-100 rounded-lg py-3 px-6 sm:text-sm xl:text-base"
+              >
                 Edit
               </button>
             </div>
